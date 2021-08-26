5d4aa3a926b230d2044229c3f4e1163260e7857a
Viet Pham <37876479+vietpham123@users.noreply.github.com>
2021-08-26 17:59:29 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,20 +12,20 @@ provider "azurerm" {
 }
 
 module "radditnetwork" {
-  source  = "app.terraform.io/Hashi-Demo/radditnetwork/azurerm"
+  source  = "app.terraform.io/vpdemo/radditnetwork/azurerm"
   version = "1.0.1"
 }
 
 module "radditvm" {
-  source  = "app.terraform.io/Hashi-Demo/radditvm/azurerm"
+  source  = "app.terraform.io/vpdemo/radditvm/azurerm"
   version = "0.9.0"
-  
+
   subnet_id = module.radditnetwork.subnet_name
 }
 
 module "radditconfig" {
-  source  = "app.terraform.io/Hashi-Demo/radditconfig/azurerm"
+  source  = "app.terraform.io/vpdemo/radditconfig/azurerm"
   version = "1.0.0"
-  
+
   host = module.radditvm.public_ip
 }
