ae75b5476ef30e5f67e529c62320562ee3ad50bd
Viet Pham <37876479+vietpham123@users.noreply.github.com>
2021-08-30 11:00:57 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ module "radditnetwork" {
 
 module "radditvm" {
   source  = "app.terraform.io/vpdemo/radditvm/azurerm"
-  version = "0.9.7"
+  version = "0.9.8"
 
   vpc_subnet = module.radditnetwork.subnet_name
   vpc_nsg = module.radditnetwork.nsg
